c543cfed75bdd194bc02bcedf8e3626fb588ed84
Markus Juenemann <markus@juenemann.net>
2016-01-30 10:11:39 +1100

    Basic tests are working.

--- gpsdshm/shm.i
+++ gpsdshm/shm.i
@@ -3,7 +3,9 @@
 %{
 #include "shm.h"
 typedef double timestamp_t;
+typedef unsigned long long int gps_mask_t;
 %}
 
 %include "shm.h"
 typedef double timestamp_t;
+typedef unsigned long long gps_mask_t;
--- tests/test_gpsdshm.py
+++ tests/test_gpsdshm.py
@@ -4,27 +4,37 @@ Tests for gpsdshm.
 These tests can be run against a real gpsd with shared memory 
 or a mock-up. This is determined by whether the `shm.fix.latitude`
 attribute returns a "real" value or 0.0. The mock-up version is
-useful when running 'python setup.py test' or testing under tox.
+useful when running 'python setup.py test' or testing under tox
+and there is no gpsd process running.
+
+The threshholds for valid values are somewhat arbitrary with the
+hope that they pass in most common scenarios. If testing against 
+a real GPS, let the GPS settle-in for a while. Also don't run
+the tests while at supersonic speed or at higher altitude than 
+Mt Everest. They are guaranteed to fail!
 
 Markus Juenemann, 29-Jan-2016
 
 """
 
 import sys
+import math
 
-from nose.tools import *
+#from nose.tools import *
 
 import gpsdshm
+import time
+import types
 
-shm = None
+gpsd_shm = None
 
 
 def setup():
-    global shm
+    global gpsd_shm
 
-    shm = gpsdshm.Shm()
+    gpsd_shm = gpsdshm.Shm()
 
-    if shm.fix.latitude <> 0.0:
+    if gpsd_shm.fix.latitude <> 0.0:
         sys.stderr.write('Using real gpsd data for tests...\n')
     else:
         sys.stderr.write('Using mock gpsd data for tests...\n')
@@ -32,172 +42,80 @@ def setup():
 
 def test_gpsdshm():
 
-    assert 
+    now = time.time()
 
+    assert isinstance(gpsd_shm.online, types.FloatType)
+    assert now-1 < gpsd_shm.online < now+1
 
-class NtpdShmTests(object):
-    unit = None
+    assert gpsd_shm.status is True
 
-    def setup(self):
-        self.shm = ntpdshm.NtpdShm(self.unit)
+    assert isinstance(gpsd_shm.skyview_time, types.FloatType)
+    assert now-1 < gpsd_shm.skyview_time < now+1 or math.isnan(gpsd_shm.skyview_time)
 
-    def teardown(self):
-        del(self.shm)
+    assert isinstance(gpsd_shm.satellites_visible, types.IntType)
+    assert 0 < gpsd_shm.satellites_visible < 73
 
-    def test_mode(self):
-        self.shm.mode = 0
-        assert self.shm.mode == 0
-        self.shm.mode = 1
-        assert self.shm.mode == 1
+    assert isinstance(gpsd_shm.fix.time, types.FloatType)
+    assert now-2 < gpsd_shm.fix.time < now+2
 
-    def test_leap(self):
-        self.shm.leap = 0
-        assert self.shm.leap == 0
-        self.shm.leap = 1
-        assert self.shm.leap == 1
+    assert gpsd_shm.fix.mode in [2,3]
 
-    def test_nsamples(self):
-        self.shm.nsamples = 0
-        assert self.shm.nsamples == 0
-        self.shm.nsamples = 1
-        assert self.shm.nsamples == 1
+    assert isinstance(gpsd_shm.fix.ept, types.FloatType)
+    assert 0.0 < gpsd_shm.fix.ept < 0.1
 
-    def test_precision(self):
-        self.shm.precision = 0
-        assert self.shm.precision == 0
-        self.shm.precision = 1
-        assert self.shm.precision == 1
+    assert isinstance(gpsd_shm.fix.latitude, types.FloatType)
+    assert -90.0 < gpsd_shm.fix.latitude < 90.0
 
-    def test_count(self):
-        self.shm.count = 0
-        assert self.shm.count == 0
-        self.shm.count = 1
-        assert self.shm.count == 1
+    assert isinstance(gpsd_shm.fix.epy, types.FloatType)
+    assert 1.0 < gpsd_shm.fix.epy < 100.0
 
-    def test_valid(self):
-        self.shm.valid = True
-        assert self.shm.valid == True
-        assert self.shm._valid == 1
+    assert isinstance(gpsd_shm.fix.longitude, types.FloatType)
+    assert -180.0 < gpsd_shm.fix.longitude < 180.0
 
-        self.shm.valid = False
-        assert self.shm.valid == False
-        assert self.shm._valid == 0
+    assert isinstance(gpsd_shm.fix.epx, types.FloatType)
+    assert 1.0 < gpsd_shm.fix.epx < 100.0
 
-        self.shm.valid = 1
-        assert self.shm.valid == True
-        assert self.shm._valid == 1
+    assert isinstance(gpsd_shm.fix.altitude, types.FloatType)
+    assert -100.0 < gpsd_shm.fix.altitude < 12000.0
 
-        self.shm.valid = 0
-        assert self.shm.valid == False
-        assert self.shm._valid == 0
+    assert isinstance(gpsd_shm.fix.epv, types.FloatType)
+    assert 1.0 < gpsd_shm.fix.epv < 100.0
 
-    def test_clockTimeStamp(self):
-        self.shm.clockTimeStamp = 12345.987654321
-        assert self.shm.clockTimeStamp == 12345.987654	          # no nansoeconds!
-        assert self.shm.clockTimeStampSec == 12345
-        assert self.shm.clockTimeStampUSec == 987654
-        assert abs(self.shm.clockTimeStampNSec - 987654321) <= 1  # conversion errors
+    assert isinstance(gpsd_shm.fix.track, types.FloatType)
+    assert 0.0 <= gpsd_shm.fix.track <= 365.0
 
-    def test_receiveTimeStamp(self):
-        self.shm.receiveTimeStamp = 54321.123456789
-        assert self.shm.receiveTimeStamp == 54321.123456            # no nansoeconds!
-        assert self.shm.receiveTimeStampSec == 54321
-        assert self.shm.receiveTimeStampUSec == 123456
-        assert abs(self.shm.receiveTimeStampNSec - 123456789) <= 1  # conversion errors
+    assert isinstance(gpsd_shm.fix.epd, types.FloatType)
+    assert 0.0 <= gpsd_shm.fix.epd <= 365 or math.isnan(gpsd_shm.fix.epd)
 
+    assert isinstance(gpsd_shm.fix.speed, types.FloatType)
+    assert 0.0 <= gpsd_shm.fix.speed <= 343.0
 
-class NtpdShmUpdateTests(object):
-    unit = None
+    assert isinstance(gpsd_shm.fix.eps, types.FloatType)
+    assert 0.0 <= gpsd_shm.fix.eps <= 343.0
 
-    def setup(self):
-        self.shm = ntpdshm.NtpdShm(self.unit)
-        self.shm.valid = False
-        self.shm.count = 0
-        self.shm.leap = 0
-        self.shm.precision = 0
-        self.shm.nsamples = 0
-        self.shm.mode = 0
+    assert isinstance(gpsd_shm.fix.climb, types.FloatType)
+    assert -100.0 < gpsd_shm.fix.climb < 100.0
 
-    def test_update(self):
-        self.shm.update(654321.214365879)
-        assert int(self.shm.receiveTimeStamp) == int(time.time())	
-        assert self.shm.clockTimeStamp == 654321.214365
-        assert self.shm.clockTimeStampSec == 654321
-        assert self.shm.clockTimeStampUSec == 214365
-        assert abs(self.shm.clockTimeStampNSec - 214365879) <= 1
-        assert self.shm.valid is True
-        assert self.shm.count == 1
-        assert self.shm.leap == 0
-        assert self.shm.precision == 0
-        assert self.shm.nsamples == 0
-        assert self.shm.mode == 0
-       
-    def test_update_leap(self):
-        self.shm.update(654322.314365879, leap=1)
-        assert self.shm.clockTimeStamp == 654322.314365
-        assert self.shm.clockTimeStampSec == 654322
-        assert self.shm.clockTimeStampUSec == 314365
-        assert abs(self.shm.clockTimeStampNSec - 314365879) <= 1
-        assert self.shm.valid is True
-        assert self.shm.count == 1
-        assert self.shm.leap == 1
-        assert self.shm.precision == 0
-        assert self.shm.nsamples == 0
-        assert self.shm.mode == 0
+    assert isinstance(gpsd_shm.fix.epc, types.FloatType)
+    assert -100.0 < gpsd_shm.fix.epc < 100.0 or math.isnan(gpsd_shm.fix.epc)
 
-    def test_update_precision(self):
-        self.shm.update(654323.414365879, precision=-1)
-        assert self.shm.clockTimeStamp == 654323.414365
-        assert self.shm.clockTimeStampSec == 654323
-        assert self.shm.clockTimeStampUSec == 414365
-        assert abs(self.shm.clockTimeStampNSec - 414365879) <= 1
-        assert self.shm.valid is True
-        assert self.shm.count == 1
-        assert self.shm.leap == 0
-        assert self.shm.precision == -1
-        assert self.shm.nsamples == 0
-        assert self.shm.mode == 0
+    assert isinstance(gpsd_shm.dop.xdop, types.FloatType)
+    assert 0.0 < gpsd_shm.dop.xdop < 2.0
 
-    def test_update_nsamples(self):
-        self.shm.update(654324.514365879, nsamples=5)
-        assert self.shm.clockTimeStamp == 654324.514365
-        assert self.shm.clockTimeStampSec == 654324
-        assert self.shm.clockTimeStampUSec == 514365
-        assert abs(self.shm.clockTimeStampNSec - 514365879) <= 1
-        assert self.shm.valid is True
-        assert self.shm.count == 1
-        assert self.shm.leap == 0
-        assert self.shm.precision == 0
-        assert self.shm.nsamples == 5
-        assert self.shm.mode == 0
+    assert isinstance(gpsd_shm.dop.ydop, types.FloatType)
+    assert 0.0 < gpsd_shm.dop.ydop < 2.0
 
-    def test_update_mode(self):
-        self.shm.update(254324.524365879, mode=6)
-        assert self.shm.clockTimeStamp == 254324.524365
-        assert self.shm.clockTimeStampSec == 254324
-        assert self.shm.clockTimeStampUSec == 524365
-        assert abs(self.shm.clockTimeStampNSec - 524365879) <= 1
-        assert self.shm.valid is True
-        assert self.shm.count == 1
-        assert self.shm.leap == 0
-        assert self.shm.precision == 0
-        assert self.shm.nsamples == 0
-        assert self.shm.mode == 6
+    assert isinstance(gpsd_shm.dop.pdop, types.FloatType)
+    assert 0.0 < gpsd_shm.dop.pdop < 2.0
 
-    def test_update_receiveTimestamp(self):
-        self.shm.update(254324.524365879, receiveTimeStamp=76543.219876543)
-        assert self.shm.clockTimeStamp == 254324.524365
-        assert self.shm.clockTimeStampSec == 254324
-        assert self.shm.clockTimeStampUSec == 524365
-        assert abs(self.shm.clockTimeStampNSec - 524365879) <= 1
-        assert self.shm.receiveTimeStamp == 76543.219876
-        assert self.shm.receiveTimeStampSec == 76543
-        assert self.shm.receiveTimeStampUSec == 219876
-        assert abs(self.shm.receiveTimeStampNSec - 219876543) <= 1
-        assert self.shm.valid is True
-        assert self.shm.count == 1
-        assert self.shm.leap == 0
-        assert self.shm.precision == 0
-        assert self.shm.nsamples == 0
-        assert self.shm.mode == 0
-        
+    assert isinstance(gpsd_shm.dop.hdop, types.FloatType)
+    assert 0.0 < gpsd_shm.dop.hdop < 2.0
+
+    assert isinstance(gpsd_shm.dop.vdop, types.FloatType)
+    assert 0.0 < gpsd_shm.dop.vdop < 2.0
+
+    assert isinstance(gpsd_shm.dop.tdop, types.FloatType)
+    assert 0.0 < gpsd_shm.dop.tdop < 2.0
+
+    assert isinstance(gpsd_shm.dop.gdop, types.FloatType)
+    assert 0.0 < gpsd_shm.dop.gdop < 3.0
